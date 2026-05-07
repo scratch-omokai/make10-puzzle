0a4172b30514a57c7d6c9ff4bc5c7186e49f45bb
P-nutsK <107553542+P-nutsK@users.noreply.github.com>
2026-05-08 00:28:43 +0900

    fix mistakes

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
-const arithmetic_symbols_checkboxes = document.querySelectorAll<HTMLInputElement>('.symbol');
+const arithmetic_symbols_checkboxes = document.querySelectorAll<HTMLInputElement>(".symbol");
 // use querySelector instead of getElementById for better Typings
-const start_button = document.querySelector<HTMLButtonElement>('#start-make')!;
-const allow_pa = document.querySelector<HTMLInputElement>('#parentheses')!;
-const allow_so = document.querySelector<HTMLInputElement>('#sort')!;
+const start_button = document.querySelector<HTMLButtonElement>("#start-make")!;
+const allow_pa = document.querySelector<HTMLInputElement>("#parentheses")!;
+const allow_so = document.querySelector<HTMLInputElement>("#sort")!;
 
-const make_num = document.querySelector<HTMLInputElement>('#make-num')!;
+const make_num = document.querySelector<HTMLInputElement>("#make-num")!;
 
-const add_num = document.querySelector<HTMLInputElement>('#add-num')!
-const remove_num = document.querySelector<HTMLInputElement>('#remove-num')!
+const add_num = document.querySelector<HTMLButtonElement>("#add-num")!;
+const remove_num = document.querySelector<HTMLButtonElement>("#remove-num")!;
 
-const ans_area = document.querySelector('#answer')!;
+const ans_area = document.querySelector("#answer")!;
 
-import { solve_make } from './solver';
+import { solve_make } from "./solver";
 
-add_num.addEventListener('click', () => {
+add_num.addEventListener("click", () => {
   const container = document.querySelector(".num-area")!;
   const input = document.createElement("input");
   input.type = "number";
@@ -23,7 +23,7 @@ add_num.addEventListener('click', () => {
   container.appendChild(input);
 });
 
-remove_num.addEventListener('click', () => {
+remove_num.addEventListener("click", () => {
   const container = document.querySelector(".num-area")!;
   const inputs = container.querySelectorAll(".target-nums");
   if (inputs.length > 1) {
@@ -31,35 +31,42 @@ remove_num.addEventListener('click', () => {
   }
 });
 
-start_button.addEventListener('click', () => {
-  const input_num = document.querySelectorAll<HTMLInputElement>('.target-nums');
+start_button.addEventListener("click", () => {
+  const input_num = document.querySelectorAll<HTMLInputElement>(".target-nums");
   ans_area.innerHTML = "計算中...";
   setTimeout(() => {
-    const seed_numbers: number[] = Array.from(input_num).map((input) => {
+    const seed_numbers: number[] = Array.from(input_num).map(input => {
       return Number(input.value);
     });
 
     const arithmetic_symbol_flags: boolean[] = Array.from(arithmetic_symbols_checkboxes).map(input => input.checked);
 
-    if(seed_numbers.some(Number.isNaN)) {
+    if (seed_numbers.some(Number.isNaN)) {
       alert("整数を正しく入力してください");
       ans_area.innerHTML = "";
       return;
     }
 
-    const results:string[] = Array.from(solve_make(seed_numbers,arithmetic_symbol_flags,[false,allow_pa.checked,allow_so.checked],Number(make_num.value)));
+    const results: string[] = Array.from(
+      solve_make(
+        seed_numbers,
+        arithmetic_symbol_flags,
+        [false, allow_pa.checked, allow_so.checked],
+        Number(make_num.value),
+      ),
+    );
     ans_area.innerHTML = "";
-    if(results.length === 0){
+    if (results.length === 0) {
       results.push("[見つかりませんでした]");
-    }else{
+    } else {
       results.unshift(`[${results.length}件見つかりました]`);
     }
 
-    for(let i = 0; i < results.length; i++){
-      const p = document.createElement('p');
+    for (let i = 0; i < results.length; i++) {
+      const p = document.createElement("p");
       p.textContent = results[i]!;
       p.className = "answer-item";
-      
+
       ans_area.appendChild(p);
     }
   }, 10);
--- src/solver.ts
+++ src/solver.ts
@@ -1,81 +1,81 @@
 import { all_permutation } from "./perm";
 
 export function solve_make(
-	nums: number[],
-	arithmetic_symbol_flags: boolean[],
-	[_unused, allow_permutation, allow_sort]: [boolean, boolean, boolean],
-	make_num: number,
+  seed_numbers: number[],
+  arithmetic_symbol_flags: boolean[],
+  [_unused, allow_permutation, allow_sort]: [boolean, boolean, boolean],
+  make_num: number,
 ): Set<string> {
-	let q: [number[], [number, number][]][] = [];
-	let fir: number[][] = [];
-	if (allow_sort) {
-		let a = all_permutation(seed_numbers);
-		for (let i = 0; i < a.length; i++) {
-			q.push([a[i]!, []]);
-			fir.push(a[i]!);
-		}
-	} else {
-		q.push([seed_numbers, []]);
-		fir.push(seed_numbers);
-	}
+  let q: [number[], [number, number][]][] = [];
+  let fir: number[][] = [];
+  if (allow_sort) {
+    let a = all_permutation(seed_numbers);
+    for (let i = 0; i < a.length; i++) {
+      q.push([a[i]!, []]);
+      fir.push(a[i]!);
+    }
+  } else {
+    q.push([seed_numbers, []]);
+    fir.push(seed_numbers);
+  }
 
-	let ans_i: [number, number][][] = [];
-	let ans_c: number[][] = [];
-	while (q.length > 0) {
-		const a: [number[], [number, number][]] = q.pop()!;
-		const c = fir.pop()!;
-		if (a[0].length === 1) {
-			if (a[0][0] === make_num) {
-				ans_i.push(a[1]);
-				ans_c.push(c);
-			}
-			continue;
-		}
-		for (let i = 0; i < a[0].length - 1; i++) {
-			for (let sy_i = 0; sy_i < 4; sy_i++) {
-				if (!arithmetic_symbol_flags[sy_i]) continue;
-				if (!(allow_permutation || a[1].length === 0)) {
-					if (a[1].slice(-1)[0]![1] <= 2 && sy_i > 2) {
-						continue;
-					} else if (a[1].slice(-1)[0]![0] > i && !(a[1].slice(-1)[0]![1] > 2 && sy_i <= 2)) {
-						continue;
-					}
-				}
+  let ans_i: [number, number][][] = [];
+  let ans_c: number[][] = [];
+  while (q.length > 0) {
+    const a: [number[], [number, number][]] = q.pop()!;
+    const c = fir.pop()!;
+    if (a[0].length === 1) {
+      if (a[0][0] === make_num) {
+        ans_i.push(a[1]);
+        ans_c.push(c);
+      }
+      continue;
+    }
+    for (let i = 0; i < a[0].length - 1; i++) {
+      for (let sy_i = 0; sy_i < 4; sy_i++) {
+        if (!arithmetic_symbol_flags[sy_i]) continue;
+        if (!(allow_permutation || a[1].length === 0)) {
+          if (a[1].slice(-1)[0]![1] <= 1 && sy_i > 1) {
+            continue;
+          } else if (a[1].slice(-1)[0]![0] > i && !(a[1].slice(-1)[0]![1] > 1 && sy_i <= 1)) {
+            continue;
+          }
+        }
 
-				let b = structuredClone(a);
-				if (sy_i === 0) b[0][i] = b[0][i]! + b[0][i + 1]!;
-				else if (sy_i === 1) b[0][i] = b[0][i]! - b[0][i + 1]!;
-				else if (sy_i === 2) b[0][i] = b[0][i]! * b[0][i + 1]!;
-				else if (sy_i === 3) {
-					if (b[0][i + 1] === 0) continue;
-					b[0][i] = b[0][i]! / b[0][i + 1]!;
-				}
-				b[0].splice(i + 1, 1);
-				b[1].push([i, sy_i]);
-				q.push(b);
-				fir.push(c);
-			}
-		}
-	}
+        let b = structuredClone(a);
+        if (sy_i === 0) b[0][i] = b[0][i]! + b[0][i + 1]!;
+        else if (sy_i === 1) b[0][i] = b[0][i]! - b[0][i + 1]!;
+        else if (sy_i === 2) b[0][i] = b[0][i]! * b[0][i + 1]!;
+        else if (sy_i === 3) {
+          if (b[0][i + 1] === 0) continue;
+          b[0][i] = b[0][i]! / b[0][i + 1]!;
+        }
+        b[0].splice(i + 1, 1);
+        b[1].push([i, sy_i]);
+        q.push(b);
+        fir.push(c);
+      }
+    }
+  }
 
-	let ans: Set<string> = new Set();
-	for (let i = 0; i < ans_i.length; i++) {
-		let tmp_ans: string[] = [];
-		for (let j of ans_c[i]!) {
-			tmp_ans.push(String(j));
-		}
+  let ans: Set<string> = new Set();
+  for (let i = 0; i < ans_i.length; i++) {
+    let tmp_ans: string[] = [];
+    for (let j of ans_c[i]!) {
+      tmp_ans.push(String(j));
+    }
 
-		for (let j of ans_i[i]!) {
-			if (j[1] === 0) tmp_ans[j[0]] = tmp_ans[j[0]] + "+" + tmp_ans[j[0] + 1];
-			else if (j[1] === 1) tmp_ans[j[0]] = tmp_ans[j[0]] + "-" + tmp_ans[j[0] + 1];
-			else if (j[1] === 2) tmp_ans[j[0]] = tmp_ans[j[0]] + "*" + tmp_ans[j[0] + 1];
-			else if (j[1] === 3) tmp_ans[j[0]] = tmp_ans[j[0]] + "/" + tmp_ans[j[0] + 1];
+    for (let j of ans_i[i]!) {
+      if (j[1] === 0) tmp_ans[j[0]] = tmp_ans[j[0]] + "+" + tmp_ans[j[0] + 1];
+      else if (j[1] === 1) tmp_ans[j[0]] = tmp_ans[j[0]] + "-" + tmp_ans[j[0] + 1];
+      else if (j[1] === 2) tmp_ans[j[0]] = tmp_ans[j[0]] + "*" + tmp_ans[j[0] + 1];
+      else if (j[1] === 3) tmp_ans[j[0]] = tmp_ans[j[0]] + "/" + tmp_ans[j[0] + 1];
 
-			tmp_ans.splice(j[0] + 1, 1);
-			if (allow_permutation && tmp_ans.length !== 1) tmp_ans[j[0]] = "(" + tmp_ans[j[0]] + ")";
-		}
+      tmp_ans.splice(j[0] + 1, 1);
+      if (allow_permutation && tmp_ans.length !== 1) tmp_ans[j[0]] = "(" + tmp_ans[j[0]] + ")";
+    }
 
-		ans.add(tmp_ans[0]!);
-	}
-	return ans;
+    ans.add(tmp_ans[0]!);
+  }
+  return ans;
 }
